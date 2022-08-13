f46c592b1d5e0b0826a0b6c8d7efd4a5625049d6
Tran Hoang Anh <anh.embedded@gmail.com>
2022-08-13 23:46:16 +0700

    Adding Module .h .c

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "u_lib/common.h"
+#include "studentsManagementSoftware/u_addModule/u_addModule.h"
+
+
 int main() {
     printf("Hello, World!\n");
     printf("This is Testing branch!\n");
