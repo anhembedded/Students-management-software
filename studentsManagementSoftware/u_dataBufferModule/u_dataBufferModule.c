4230b3f0e36fdafa30e6d6dad0921eeb861b628e
Tran Hoang Anh <anh.embedded@gmail.com>
2022-08-14 09:58:27 +0700

    testing Input Data structure

--- studentsManagementSoftware/u_dataBufferModule/u_dataBufferModule.c
+++ studentsManagementSoftware/u_dataBufferModule/u_dataBufferModule.c
@@ -6,5 +6,4 @@
 
 student_T dataBuffer[1000];
 fullName_T fullNameBuffer_ptr[1000];
-
- st_Name_T fullNameBuffer_data[1000];
+st_Name_T fullNameBuffer_data[1000];
--- studentsManagementSoftware/u_dataBufferModule/u_dataBufferModule.h
+++ studentsManagementSoftware/u_dataBufferModule/u_dataBufferModule.h
@@ -5,6 +5,7 @@
 #ifndef STUDENTS_MANAGEMENT_SOFTWARE_U_DATABUFFERMODULE_H
 #define STUDENTS_MANAGEMENT_SOFTWARE_U_DATABUFFERMODULE_H
 #include "studentsManagementSoftware/u_dataStruct.h"
+
 extern student_T dataBuffer[];
 extern fullName_T fullNameBuffer_ptr[];
 typedef struct st_Name
